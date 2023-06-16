e521347edb54b8bf1db0bf7e8f4f0ec20029b30a
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2023-06-16 14:34:30 -0400

    New cross-widget functions and styles

---
+++ widgets/widget.js
@@ -0,0 +1,15 @@
+/*! 
+* SmallWorlds X JavaScript Common Widget
+* Description: Handle common functions and elements shared across widgets.
+* Author: Benjamin Robert
+* Author: Justin Schwertmann
+* URI: https://www.smallworlds.app/
+* Created: 6/15/23
+* Last Edited: 6/15/23
+*/
+
+function widgetOnAPILoad(){
+	
+	document.getElementById("loadingOverlay").className = "widget-overlay widget-overlay-dismissed";
+	
+}
