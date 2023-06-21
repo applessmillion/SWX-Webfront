e231373e9e491dddcde36ebb8b29a12ab859e49f
Ben Robert <appless9009@gmail.com>
2023-06-21 16:05:02 -0400

    Create interactive placeholder for S2W
    We're getting there but still need to hook into the API.
    Widget view is complete
    Added ho tomepage

--- widgets/spin/s2wApi.js
+++ widgets/spin/s2wApi.js
@@ -36,7 +36,7 @@ async function apiConnectS2W(){
 	// API is unreachable. Handle running locally without any calls.
 	if(!has_api_online){
 		// Display red top-bar to indicate an error has occured.
-		// document.getElementById("topUI").style.background = "#ff5b5b";
+		document.getElementById("topUI").style.background = "#ff5b5b";
 		
 	}
 	// API has been reached. Connect and get details.
@@ -49,30 +49,8 @@ async function apiConnectS2W(){
 	widgetOnAPILoad(has_api_online);
 }
 
-// Get details on requested avatar's pet.
-// Default is set to 'Benjo ' avatar ID in case of error.
+// Get details on requested avatar's s2w state.
 async function s2wLoadApiDetails(meID){
-	
-	// This block of code will be to determine if our variable my_pet_info is defined.
-	// Currently, this is handled and set from the homepage re-write. It is not set anywhere in our widget.
-	// Eventually, we will move this variable declaration to this widget, probably here. 
-	// For now, just get this feature-ful, it will need to be run from the homepage.
-	
-	// Utilize global variable api_user_info to pull avatar data & pet data.
-		await getAPIresponse('avatar/pet/'+ownerId+"/");
-		let my_pet_info = response_data;
-		// Merge db data - stringId+headPostfix
-		pet_pic_string = response_data.stringid+response_data.postfix;
-			
-		// Build links to pet images
-		let pet_snapshot = 'https://avatars.smallworlds.app/'+pet_pic_string+'_snap.png';
-		let pet_thumbnail = 'https://avatars.smallworlds.app/'+pet_pic_string+'_thumb.png';
-		let pet_head = 'https://avatars.smallworlds.app/'+pet_pic_string+'.png';
-		
-		
-		document.getElementById('petName').innerHTML = my_pet_info.name;
-		document.getElementById('petMotto').innerHTML = my_pet_info.desc;
-		document.getElementById('petHead').style.background = 'url('+pet_head+')';
 }
 	
 
