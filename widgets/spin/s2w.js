77bb71b77e4d58bff75cae863b93c30072e8786e
Ben Robert <34635617+applessmillion@users.noreply.github.com>
2023-07-15 19:30:36 -0400

    Add click functionality to both wheels
    When clicked, does the following:
    Pauses CSS animation & pushes rotation to JS.
    Hides other wheel & moves bottomUI to off-screen.
    Removes all potentially conflicting event listeners.
    Fades-in button menu to handle spins/purchasing spins

--- widgets/spin/s2w.js
+++ widgets/spin/s2w.js
@@ -5,7 +5,7 @@
 * Author: Justin Schwertmann
 * URI: https://www.smallworlds.app/
 * Created: 7/10/23
-* Last Edited: 7/10/23
+* Last Edited: 7/15/23
 */
 
 // ----- VARIABLES
@@ -15,7 +15,7 @@ let s2w_d_buy1 = []; // Purchase option from the database. Contains number of sp
 let s2w_d_buy2 = []; // Purchase option from the database. Contains number of spins [0] in option, and cost [1] 
 let s2w_d_buy3 = []; // Purchase option from the database. Contains number of spins [0] in option, and cost [1] 
 let s2w_d_buy4 = []; // Test a web exclusive option. Contains number of spins [0] in option, and cost [1] 
-
+let s2w_degree_guesser; // Use JS to guess where the CSS animation is for both wheels.
 
 // ----- FUNCTIONS
 
@@ -95,11 +95,36 @@ function s2wBasicWheelInit(){
 	document.getElementById('wheelTileDeluxe').removeEventListener('mouseenter', s2wDeluxeWheelFocus);
 	document.getElementById('wheelTileDeluxe').removeEventListener('mouseleave', s2wClearWidgetStyles);
 	
-	
 	// Hide other UI
 	document.querySelector("div#bottomUI").style.setProperty('opacity', '0');
 	document.querySelector("#wheelTileDeluxe").style.setProperty('opacity', '0');
 	document.querySelector("#s2wLogoDeluxe").style.setProperty('opacity', '0');
+	document.querySelector("div#bottomUI").style.setProperty('bottom', '-50px');
+	// Transition wheel from css animation to JS-enforced transform style.
+	clearInterval(s2w_degree_guesser);
+	document.querySelector("div#wheelTileBasic div.wheel-element").style.setProperty('animation', 'none');
+	document.querySelector("div#wheelTileBasic div.wheel-element").style.setProperty('transition', '1s ease-out');
+	document.querySelector("div#wheelTileBasic div.wheel-element").style.setProperty('transform', 'rotate('+ widget_s2w_basic_deg +'deg)');
+
+	// Transition wheel to a full stop, 
+	setTimeout(function(){
+		widget_s2w_basic_deg += 9;
+		document.querySelector("div#wheelTileBasic div.wheel-element").style.setProperty('transform', 'rotate('+ widget_s2w_basic_deg +'deg)');
+	},35);
+	
+	// Display s2w button menu to the right. Hide elements that do not pertain to s2w basic.
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('opacity', '1');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('position','absolute');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('right','10px');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('left','');
+	document.querySelector('div.s2w-play-btn-container button#basicPlay').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container h2#deluxePayTitle').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container button#deluxePlay').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay1').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay2').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay3').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay4').style.setProperty('display','none');
 }
 
 function s2wDeluxeWheelInit(){
@@ -109,15 +134,43 @@ function s2wDeluxeWheelInit(){
 	document.getElementById('wheelTileDeluxe').removeEventListener('mouseenter', s2wDeluxeWheelFocus);
 	document.getElementById('wheelTileDeluxe').removeEventListener('mouseleave', s2wClearWidgetStyles);
 	
-	
 	// Hide other UI
 	document.querySelector("div#bottomUI").style.setProperty('opacity', '0');
 	document.querySelector("#wheelTileBasic").style.setProperty('opacity', '0');
 	document.querySelector("#s2wLogo").style.setProperty('opacity', '0');
+	document.querySelector("div#bottomUI").style.setProperty('bottom', '-50px');
+	
+	// Transition wheel from css animation to JS-enforced transform style.
+	clearInterval(s2w_degree_guesser);
+	document.querySelector("div#wheelTileDeluxe div.wheel-element").style.setProperty('animation', 'none');
+	document.querySelector("div#wheelTileDeluxe div.wheel-element").style.setProperty('transition', '1s ease-out');
+	document.querySelector("div#wheelTileDeluxe div.wheel-element").style.setProperty('transform', 'rotate('+ widget_s2w_deluxe_deg +'deg)');
+
+	// Transition wheel to a full stop, 
+	setTimeout(function(){
+		widget_s2w_deluxe_deg -= 9;
+		document.querySelector("div#wheelTileDeluxe div.wheel-element").style.setProperty('transform', 'rotate('+ widget_s2w_deluxe_deg +'deg)');
+	},35);
+	
+	// Display s2w button menu to the right. Hide elements that do not pertain to s2w basic.
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('opacity', '1');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('position','absolute');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('left','10px');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('right','');
+	document.querySelector('div.s2w-play-btn-container button#basicPlay').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container h2#deluxePayTitle').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container button#deluxePlay').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay1').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay2').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay3').style.setProperty('display','');
+	document.querySelector('div.s2w-play-btn-container button#deluxePay4').style.setProperty('display','');
 }
 
 function s2wOnLoad(){
 	document.querySelector('div.s2w-play-btn-container').style.setProperty('display','none');
+	document.querySelector('div.s2w-play-btn-container').style.setProperty('opacity', '0');
+	s2w_degree_guesser = setInterval(s2wGuessWheelDeg, 1000); // Use JS to guess where the CSS animation is for both wheels.
 }
 
 // Remove all style elements from objects in widget.
@@ -128,6 +181,20 @@ function s2wClearWidgetStyles(){
 	document.querySelector("div#wheelTileDeluxe").style='';
 	document.querySelector("div#bottomUI").style='';
 	document.querySelector("#s2wLogoDeluxe").style='';
-	document.querySelector("#s2wLogo").style='';
+	document.querySelector("#s2wLogo").style='';	
+}
+
+
+// Remove focus from wheel and revert back to idling.
+function s2wRevertWheelInit(){
+	s2wClearWidgetStyles();
+	s2wOnLoad();
 	
+	document.getElementById('wheelTileBasic').addEventListener('click', s2wBasicWheelInit);
+	document.getElementById('wheelTileBasic').addEventListener('mouseleave', s2wClearWidgetStyles);
+	document.getElementById('wheelTileBasic').addEventListener('mouseenter', s2wBasicWheelFocus);
+	
+	document.getElementById('wheelTileDeluxe').addEventListener('click', s2wDeluxeWheelInit);
+	document.getElementById('wheelTileDeluxe').addEventListener('mouseleave', s2wClearWidgetStyles);
+	document.getElementById('wheelTileDeluxe').addEventListener('mouseenter', s2wDeluxeWheelFocus);
 }
